2ad1fea7701eee8b428ce1c6c06c597986d7a491
ArmanKhanikiryan <akhanikiryan@gmail.com>
2023-01-22 01:20:24 +0400

    material UI

--- Back-End Shop/Front-End Shop/src/App.tsx
+++ Back-End Shop/Front-End Shop/src/App.tsx
@@ -1,19 +1,53 @@
-import React, {FC} from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import {Button, TextField} from '@mui/material'
+import ResponsiveAppBar from "./AppBar";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+const theme = createTheme({
+    palette: {
+        primary: {
+            light: '#ff7961',
+            main: '#f44336',
+            dark: '#ba000d',
+            contrastText: '#000',
+        },
+        secondary: {
+            light: '#ff7961',
+            main: '#f44336',
+            dark: '#ba000d',
+            contrastText: '#000',
+        },
+    },
+});
 
 
-type TApp = {
-  arman: string,
-  abo: string
-}
+
+const  App = () => {
+    const [darkMode, setDarkMode] = useState<boolean>(false)
 
 
-const  App:FC<TApp> = ({arman, abo}) => {
+    const darkTheme = createTheme({
+        palette: {
+            mode: `${darkMode ? 'dark' : 'light'}`,
+        },
+    });
 
 
   return (
     <div className="App">
-
+        <button onClick={() => setDarkMode(!darkMode)}>Toggle Mode</button>
+        <ThemeProvider theme={darkTheme}>
+            <CssBaseline />
+            <main>This app is using the dark mode</main>
+        </ThemeProvider>
+        <ResponsiveAppBar/>
+      <Button color='primary' variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+        <TextField color={'primary'} id="outlined-basic" label="Outlined" variant="outlined" />
+        <TextField id="filled-basic" label="Filled" variant="filled" />
+        <TextField id="standard-basic" label="Standard" variant="standard" />
 
     </div>
   );
